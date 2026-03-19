34850da2adbaae65014688aee802976838eb0256
Katie-Lui <katielui51@gmail.com>
2026-03-19 00:30:27 -0400

    made red filter and index html
    (not tested)

--- color_filter.js
+++ color_filter.js
@@ -11,13 +11,20 @@ fileInput.addEventListener("change",
         reader.addEventListener("load", 
             function() {
                 imagePreview.src = this.result;
-                imagePreview.style.diaplay = "block";
+                imagePreview.style.display = "block";
             }
         );
 
         reader.readAsDataURL(file);
     }
 });
+
+let filter = document.getElementById("filter");
+
+/* i would like to make the alpha value (and maybe even all the color values) adjustable via range picker or color picer circle thingyyy */
+function red() {
+    filter.style.backgroundColor = (255, 0, 0, 0.5)
+}
 
 /*function redFilter {
 
